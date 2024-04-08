3dc2b4837b6f095bce808f5b4d2be39ba5654115
Angelo <avagenas14@gmail.com>
2024-04-08 12:53:27 -0400

    adding ios

--- src/index.js
+++ src/index.js
@@ -10,38 +10,64 @@ const config = {
         {
           destination: "variables.css",
           format: "css/variables",
+          options: {
+            outputReferences: true,
+          },
+        },
+      ],
+    },
+    ios: {
+      transformGroup: "ios",
+      buildPath: "build/ios/",
+      files: [
+        {
+          destination: "StyleDictionaryColor.h",
+          format: "ios/colors.h",
+          className: "StyleDictionaryColor",
+          type: "StyleDictionaryColorName",
+          filter: {
+            attributes: {
+              category: "color",
+            },
+          },
+        },
+        {
+          destination: "StyleDictionaryColor.m",
+          format: "ios/colors.m",
+          className: "StyleDictionaryColor",
+          type: "StyleDictionaryColorName",
+          filter: {
+            attributes: {
+              category: "color",
+            },
+          },
+        },
+        {
+          destination: "StyleDictionarySize.h",
+          format: "ios/static.h",
+          className: "StyleDictionarySize",
+          type: "float",
+          filter: {
+            attributes: {
+              category: "size",
+            },
+          },
+        },
+        {
+          destination: "StyleDictionarySize.m",
+          format: "ios/static.m",
+          className: "StyleDictionarySize",
+          type: "float",
+          filter: {
+            attributes: {
+              category: "size",
+            },
+          },
         },
       ],
     },
   },
 };
-
-/**
- *
- * CUSTOM TRANSFORM EXAMPLE
- * JSON SCHEMA
- *
-  {
-    "test": {
-      "small": { "$type": "strToNumber", "$value": "12" },
-      "med": { "$type": "strToNumber", "$value": "14" },
-      "large" : { "$value": "{test.small}" },
-      "xlarge" : { "$value": "{test.med}" }
-    }
-  }
-  *
- */
-
-StyleDictionary.registerTransform({
-  name: "transform/strToNumber",
-  type: `value`,
-  matcher: (token) => token?.$type === "strToNumber",
-  transitive: true,
-  transformer:
-    () =>
-    ({ $value }) =>
-      Number($value),
-});
 
 const sd = StyleDictionary.extend(config);
 sd.buildAllPlatforms();
